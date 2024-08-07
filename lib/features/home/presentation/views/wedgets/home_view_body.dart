a942b211c1c557a0b365eacdebfd9a0081d03ef8
sama <samaabdelghfar90@gmail.com>
2024-08-07 08:12:18 +0200

    add custom app bar to home

---
+++ lib/features/home/presentation/views/wedgets/home_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:bookapp/features/home/presentation/views/wedgets/custom_appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
+

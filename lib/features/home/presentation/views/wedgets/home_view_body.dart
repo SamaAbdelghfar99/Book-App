55c33277fb8c76d17b5289c47917ad1156d25fb8
sama <samaabdelghfar90@gmail.com>
2024-08-07 11:18:17 +0200

    create styles file and title to home view

--- lib/features/home/presentation/views/wedgets/home_view_body.dart
+++ lib/features/home/presentation/views/wedgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookapp/features/home/presentation/views/wedgets/custom_appbar.dart';
 import 'package:bookapp/features/home/presentation/views/wedgets/custom_list_view_item.dart';
+import 'package:bookapp/features/home/presentation/views/wedgets/feature_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -10,29 +11,23 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
         FeaturedBooksListView(),
+        SizedBox(
+          height: 40,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('Best Saller',style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),),
+        )
       ],
     );
   }
 }
 
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height *.3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context,index){
-        return const Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 8),
-          child:  CustomListViewIcon(),
-        );
-      },),
-    );
-  }
-}
